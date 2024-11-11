124fe6917c42a88facebd5f5e3d461add22ea34c
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-11-11 13:20:59 -0300

    Convierto uuid en node fetch_dspacedb

--- src/kedro_cic/pipelines/dspacedb/nodes.py
+++ src/kedro_cic/pipelines/dspacedb/nodes.py
@@ -9,10 +9,25 @@ def fetch_dspacedb(item, metadatavalue, metadatafieldregistry, metadataschemareg
     df_metadatavalue = metadatavalue
     df_metadatafieldregistry = metadatafieldregistry
     df_metadataschemaregistry = metadataschemaregistry
+    
+    # convert df_item dtypes
     df_item = df_item.convert_dtypes()
+    df_item['uuid'] = df_item['uuid'].astype(str)
+    
+    df_item['submitter_id'] = df_item['submitter_id'].astype(str)
+    df_item['owning_collection'] = df_item['owning_collection'].astype(str)
+
+    # convert df_metadatavalue dtypes
     df_metadatavalue = df_metadatavalue.convert_dtypes()
+    df_metadatavalue['dspace_object_id'] = df_metadatavalue['dspace_object_id'].astype(str)
+
+    # convert df_metadatafieldregistry dtypes
     df_metadatafieldregistry = df_metadatafieldregistry.convert_dtypes()
+
+    # convert df_metadataschemaregistry dtypes
     df_metadataschemaregistry = df_metadataschemaregistry.convert_dtypes()
+    df_metadatafieldregistry = df_metadatafieldregistry.convert_dtypes()
+
     df_item['load_datetime'] = date.today()
     df_metadatavalue['load_datetime'] = date.today()
     df_metadatafieldregistry['load_datetime'] = date.today()
