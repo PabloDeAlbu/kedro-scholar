e5b74841580c5a7dd1251c6e3d7e86943e934779
Pablo de Albuquerque <pablo.cda@gmail.com>
2024-12-12 13:53:56 -0300

    Recupero mas tablas de dspacedb

--- src/kedro_cic/pipelines/dspacedb/nodes.py
+++ src/kedro_cic/pipelines/dspacedb/nodes.py
@@ -4,35 +4,27 @@ generated using Kedro 0.19.8
 """
 from datetime import date
 
-def fetch_dspacedb(item, metadatavalue, metadatafieldregistry, metadataschemaregistry):
-    df_item = item
-    df_metadatavalue = metadatavalue
-    df_metadatafieldregistry = metadatafieldregistry
-    df_metadataschemaregistry = metadataschemaregistry
-    
-    # convert df_item dtypes
-    df_item = df_item.convert_dtypes()
-    df_item['uuid'] = df_item['uuid'].astype(str)
-    
-    df_item['submitter_id'] = df_item['submitter_id'].astype(str)
-    df_item['owning_collection'] = df_item['owning_collection'].astype(str)
-
-    # convert df_metadatavalue dtypes
-    df_metadatavalue = df_metadatavalue.convert_dtypes()
-    df_metadatavalue['dspace_object_id'] = df_metadatavalue['dspace_object_id'].astype(str)
-
-    # convert df_metadatafieldregistry dtypes
-    df_metadatafieldregistry = df_metadatafieldregistry.convert_dtypes()
-
-    # convert df_metadataschemaregistry dtypes
-    df_metadataschemaregistry = df_metadataschemaregistry.convert_dtypes()
-    df_metadatafieldregistry = df_metadatafieldregistry.convert_dtypes()
-
+def fetch_dspacedb(df_bitstream, df_bundle, df_bundle2bitstream, df_collection, df_collection2item, df_community, df_community2collection, df_community2community, df_doi, df_eperson, df_epersongroup, df_epersongroup2eperson, df_group2group, df_handle, df_item, df_item2bundle, df_metadatafieldregistry, df_metadataschemaregistry, df_metadatavalue):
+    df_bitstream['load_datetime'] = date.today()
+    df_bundle['load_datetime'] = date.today()
+    df_bundle2bitstream['load_datetime'] = date.today()
+    df_collection['load_datetime'] = date.today()
+    df_collection2item['load_datetime'] = date.today()
+    df_community['load_datetime'] = date.today()
+    df_community2collection['load_datetime'] = date.today()
+    df_community2community['load_datetime'] = date.today()
+    df_doi['load_datetime'] = date.today()
+    df_eperson['load_datetime'] = date.today()
+    df_epersongroup['load_datetime'] = date.today()
+    df_epersongroup2eperson['load_datetime'] = date.today()
+    df_group2group['load_datetime'] = date.today()
+    df_handle['load_datetime'] = date.today()
     df_item['load_datetime'] = date.today()
-    df_metadatavalue['load_datetime'] = date.today()
+    df_item2bundle['load_datetime'] = date.today()
     df_metadatafieldregistry['load_datetime'] = date.today()
     df_metadataschemaregistry['load_datetime'] = date.today()
-    return df_item, df_metadatavalue, df_metadatafieldregistry, df_metadataschemaregistry
+    df_metadatavalue['load_datetime'] = date.today()
+    return df_bitstream, df_bundle, df_bundle2bitstream, df_collection, df_collection2item, df_community, df_community2collection, df_community2community, df_doi, df_eperson, df_epersongroup, df_epersongroup2eperson, df_group2group, df_handle, df_item, df_item2bundle, df_metadatafieldregistry, df_metadataschemaregistry, df_metadatavalue
 
 def land_dspacedb(item, metadatavalue, metadatafieldregistry, metadataschemaregistry):
     df_item = item
