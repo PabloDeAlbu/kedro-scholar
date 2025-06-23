6ef7a40606cfe0574b798d421261ce5a1fb21d20
Pablo de Albuquerque <pablo.cda@gmail.com>
2025-06-23 08:03:08 -0300

    Refactor de notebooks separando en marts y sources. Cambio nombre para nodos en oai

--- src/kedro_cic/pipelines/oai/pipeline.py
+++ src/kedro_cic/pipelines/oai/pipeline.py
@@ -1,15 +1,18 @@
 from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import (
-    fetch_item_oai, 
-    land_item_oai
+    fetch_oai_items,
+    fetch_oai_item_by_set,
+    fetch_oai_sets,
+    land_items_oai,
+    land_oai_item_by_set,
     )
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            name="fetch_item_oai",
-            func=fetch_item_oai,
+            name="fetch_oai_items",
+            func=fetch_oai_items,
             inputs=[
                 "params:oai_fetch_options.base_url",
                 "params:oai_fetch_options.context",
@@ -18,8 +21,33 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs="raw/oai/item#parquet"
         ),
         node(
-            name="land_item_oai",
-            func=land_item_oai,
+            name="fetch_oai_sets",
+            func=fetch_oai_sets,
+            inputs=[
+                "params:oai_fetch_options.base_url",
+                "params:oai_fetch_options.context",
+                "params:fetch_options.env",
+            ],
+            outputs="raw/oai/item#csv"
+        ),
+        node(
+            name="fetch_oai_item_by_set",
+            func=fetch_oai_item_by_set,
+            inputs=[
+                "params:oai_fetch_options.base_url",
+                "params:oai_fetch_options.context",
+                "params:oai_fetch_options.set_id",
+                "params:oai_fetch_options.metadata_format",
+                "params:fetch_options.env",
+            ],
+            outputs=[
+                "raw/oai/item_by_set#parquet",
+                "raw/oai/item_by_set_dev#csv"
+                ]
+        ),
+        node(
+            name="land_items_oai",
+            func=land_items_oai,
             inputs="raw/oai/item#parquet",
             outputs=[
                 "ldg/oai/item",
@@ -28,6 +56,19 @@ def create_pipeline(**kwargs) -> Pipeline:
                 "ldg/oai/item2language",
                 "ldg/oai/item2subject",
                 "ldg/oai/item2relation"
+            ],
+        ),
+        node(
+            name="land_oai_item_by_set",
+            func=land_oai_item_by_set,
+            inputs="raw/oai/item_by_set#parquet",
+            outputs=[
+                "ldg/oai/item_by_set",
+                "ldg/oai/item_creator",
+                "ldg/oai/item_language",
+                "ldg/oai/item_subject",
+                "ldg/oai/item_relation",
+                "ldg/oai/item_publisher"
             ],
         ),
     ], tags="oai"
